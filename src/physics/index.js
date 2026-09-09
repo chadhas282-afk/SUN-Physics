87485595794ddf159a11e492987f6d5f5e4ba3a7
Sahil Chadha <Chadhas282@gmail.com>
2026-09-09 10:35:38 +0530

    Add physics module exports for various calculations

---
+++ src/physics/index.js
@@ -0,0 +1,8 @@
+export { calculateBouncingBall } from './bouncingBall.js';
+export { calculateSpring } from './spring.js';
+export { calculatePendulum } from './pendulum.js';
+export { calculateProjectile2D } from './projectile2D.js';
+export { calculateFriction } from './friction.js';
+export { calculateOrbital } from './orbital.js';
+export { calculateFluidDrag } from './fluidDrag.js';
+export { calculateParticles } from './particles.js';
