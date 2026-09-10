5724ef7380445f24dbed6caa374ccfc0a2eb792b
Sahil Chadha <Chadhas282@gmail.com>
2026-09-10 15:37:28 +0530

    Add ControlPanel component with props for managing application controls

--- src/components/ControlPanel.jsx
+++ src/components/ControlPanel.jsx
@@ -7,3 +7,14 @@ const Tooltip = ({ text }) => (
     <span className="tooltip-text">{text}</span>
   </span>
 );
+
+export default function ControlPanel({ 
+  activeConcept, 
+  onOpenModal,
+  params, 
+  setParams, 
+  compareMode, 
+  setCompareMode,
+  onGenerate,
+  isGenerating
+}) {
